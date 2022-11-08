9b3ebf74757b2dab0c34db7d3f71e6b88436ab23
mgasoftware <macir.gassama12@gmail.com>
2022-11-08 11:00:27 +0100

    Add css for cards to adjust the render

--- src/components/Features/Cards.js
+++ src/components/Features/Cards.js
@@ -9,12 +9,14 @@ function Cards(props) {
         <div className="kasa-main">
             <Banner background={background} />
             <main className="kasa-mainCard">
-                {props.logements.map((logement, index) => (
-                    <Link key={index} to={"/logement/"+logement.id} className='kasa-mainCardBody'>
-                        <img src={logement.cover} alt={logement.title} className="kasa-mainCardImg" />
-                        <h2 className="kasa-mainCardTitle">{logement.title}</h2>
-                    </Link>
-                ))}
+                <div className='kasa-mainCardCenter'>
+                    {props.logements.map((logement, index) => (
+                        <Link key={index} to={"/logement/" + logement.id} className='kasa-mainCardBody'>
+                            <img src={logement.cover} alt={logement.title} className="kasa-mainCardImg" />
+                            <h2 className="kasa-mainCardTitle">{logement.title}</h2>
+                        </Link>
+                    ))}
+                </div>
             </main>
         </div>
     )
